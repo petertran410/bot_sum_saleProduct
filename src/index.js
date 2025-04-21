4e50a9389ff76cfb8aa34bec900fe6f58369848e
Ngoc Nhan <60886911+petertran410@users.noreply.github.com>
2025-04-21 10:45:03 +0700

    Updating

--- src/index.js
+++ src/index.js
@@ -112,6 +112,6 @@ app.get("/convert-to-excel", (req, res) => {
 
 // Khởi động server
 app.listen(PORT, () => {
-  scheduler.startScheduler(15, 1);
+  scheduler.startScheduler(15, 160);
   // scheduler.scheduleExcelConversion(60);
 });
