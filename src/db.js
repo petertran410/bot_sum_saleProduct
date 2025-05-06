51be90e74ae167669731f0b3287fb0ed805b118f
Ngoc Nhan <60886911+petertran410@users.noreply.github.com>
2025-05-06 14:09:33 +0700

    Fix saving historical data

--- src/db.js
+++ src/db.js
@@ -1,47 +1,10 @@
-const mysql = require("mysql2/promise");
-require("dotenv").config();
-
-const dbConfig = {
-  host: "localhost",
-  user: "admin",
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME || "kiotviet_data",
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0,
-};
-
-const pool = mysql.createPool(dbConfig);
-
-async function testConnection() {
-  try {
-    const connection = await pool.getConnection();
-    connection.release();
-    return true;
-  } catch (error) {
-    console.error("Database connection failed:", error);
-    return false;
-  }
-}
-
-function getPool() {
-  return pool;
-}
-
-module.exports = {
-  getPool,
-  testConnection,
-};
-
 // const mysql = require("mysql2/promise");
 // require("dotenv").config();
 
-// // Cập nhật thông tin kết nối đến MariaDB trên NAS Synology
 // const dbConfig = {
-//   host: process.env.DB_HOST || "192.168.1.200",
-//   port: process.env.DB_PORT || 3306,
-//   user: process.env.DB_USER || "root",
-//   password: process.env.DB_PASSWORD || "Dieptra@123",
+//   host: "localhost",
+//   user: "admin",
+//   password: process.env.DB_PASSWORD,
 //   database: process.env.DB_NAME || "kiotviet_data",
 //   waitForConnections: true,
 //   connectionLimit: 10,
@@ -53,11 +16,10 @@ module.exports = {
 // async function testConnection() {
 //   try {
 //     const connection = await pool.getConnection();
-//     console.log("Kết nối đến MariaDB trên NAS Synology thành công");
 //     connection.release();
 //     return true;
 //   } catch (error) {
-//     console.error("Kết nối đến MariaDB trên NAS Synology thất bại:", error);
+//     console.error("Database connection failed:", error);
 //     return false;
 //   }
 // }
@@ -70,3 +32,41 @@ module.exports = {
 //   getPool,
 //   testConnection,
 // };
+
+const mysql = require("mysql2/promise");
+require("dotenv").config();
+
+// Cập nhật thông tin kết nối đến MariaDB trên NAS Synology
+const dbConfig = {
+  host: process.env.DB_HOST || "192.168.1.200",
+  port: process.env.DB_PORT || 3306,
+  user: process.env.DB_USER || "root",
+  password: process.env.DB_PASSWORD || "Dieptra@123",
+  database: process.env.DB_NAME || "kiotviet_data",
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+};
+
+const pool = mysql.createPool(dbConfig);
+
+async function testConnection() {
+  try {
+    const connection = await pool.getConnection();
+    console.log("Kết nối đến MariaDB trên NAS Synology thành công");
+    connection.release();
+    return true;
+  } catch (error) {
+    console.error("Kết nối đến MariaDB trên NAS Synology thất bại:", error);
+    return false;
+  }
+}
+
+function getPool() {
+  return pool;
+}
+
+module.exports = {
+  getPool,
+  testConnection,
+};
