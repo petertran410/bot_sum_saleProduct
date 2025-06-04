2ce35e53bf775148bce8724396c66234c45f5d7e
Ngoc Nhan <60886911+petertran410@users.noreply.github.com>
2025-06-05 01:42:49 +0700

    Add lark commit

--- src/index.js
+++ src/index.js
@@ -35,7 +35,7 @@ app.use((req, res, next) => {
 });
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*"); // In production, replace * with your domain
+  res.header("Access-Control-Allow-Origin", "https://api.traphuonghoang.com"); // In production, replace * with your domain
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   res.header(
     "Access-Control-Allow-Headers",
